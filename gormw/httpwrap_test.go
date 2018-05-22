e6114feb24473f69962c58a1d5f334998ad83d07
amyangfei <amyangfei@gmail.com>
2018-05-22 23:21:04 +0800

    Fix import cycle in test case

--- gormw/httpwrap_test.go
+++ gormw/httpwrap_test.go
@@ -2,14 +2,13 @@ package gormw
 
 import (
 	"fmt"
-	"github.com/amyangfei/gor_middleware/gormw"
 	"reflect"
 	"testing"
 )
 
 func TestHttpMethod(t *testing.T) {
 	payload := "GET /test HTTP/1.1\r\n\r\n"
-	if method, err := gormw.HttpMethod(payload); err != nil {
+	if method, err := HttpMethod(payload); err != nil {
 		t.Errorf(err.Error())
 	} else if method != "GET" {
 		t.Errorf("invalid method: %s", method)
@@ -18,19 +17,19 @@ func TestHttpMethod(t *testing.T) {
 
 func TestHttpPath(t *testing.T) {
 	payload := "GET /test HTTP/1.1\r\n\r\n"
-	if path, err := gormw.HttpPath(payload); err != nil {
+	if path, err := HttpPath(payload); err != nil {
 		t.Errorf(err.Error())
 	} else if path != "/test" {
 		t.Errorf("invalid path: %s", path)
 	}
 
-	if new_payload, err := gormw.SetHttpPath(payload, "/"); err != nil {
+	if new_payload, err := SetHttpPath(payload, "/"); err != nil {
 		t.Errorf(err.Error())
 	} else if new_payload != "GET / HTTP/1.1\r\n\r\n" {
 		t.Errorf("invalid new_payload: %s", new_payload)
 	}
 
-	if new_payload, err := gormw.SetHttpPath(payload, "/new/test"); err != nil {
+	if new_payload, err := SetHttpPath(payload, "/new/test"); err != nil {
 		t.Errorf(err.Error())
 	} else if new_payload != "GET /new/test HTTP/1.1\r\n\r\n" {
 		t.Errorf("invalid new_payload: %s", new_payload)
@@ -39,27 +38,27 @@ func TestHttpPath(t *testing.T) {
 
 func TestHttpPathParam(t *testing.T) {
 	payload := "GET /test HTTP/1.1\r\n\r\n"
-	if params, err := gormw.HttpPathParam(payload, "test"); err != nil {
+	if params, err := HttpPathParam(payload, "test"); err != nil {
 		t.Errorf(err.Error())
 	} else if !reflect.DeepEqual(params, []string{}) {
 		t.Errorf("invalid params: %v", params)
 	}
 
-	payload, err := gormw.SetHttpPathParam(payload, "test", "123")
+	payload, err := SetHttpPathParam(payload, "test", "123")
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	if params, err := gormw.HttpPathParam(payload, "test"); err != nil {
+	if params, err := HttpPathParam(payload, "test"); err != nil {
 		t.Errorf(err.Error())
 	} else if !reflect.DeepEqual(params, []string{"123"}) {
 		t.Errorf("invalid params: %v", params)
 	}
 
-	payload, err = gormw.SetHttpPathParam(payload, "qwer", "ty")
+	payload, err = SetHttpPathParam(payload, "qwer", "ty")
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	if params, err := gormw.HttpPathParam(payload, "qwer"); err != nil {
+	if params, err := HttpPathParam(payload, "qwer"); err != nil {
 		t.Errorf(err.Error())
 	} else if !reflect.DeepEqual(params, []string{"ty"}) {
 		t.Errorf("invalid params: %v", params)
@@ -74,7 +73,7 @@ func TestHttpHeader(t *testing.T) {
 		"Content-Length": "5",
 	}
 	for name, value := range expected {
-		header, err := gormw.HttpHeader(payload, name)
+		header, err := HttpHeader(payload, name)
 		if err != nil {
 			t.Errorf(err.Error())
 		}
@@ -89,7 +88,7 @@ func TestSetHttpHeader(t *testing.T) {
 	uas := []string{"", "1", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0)"}
 	expected := "GET / HTTP/1.1\r\nUser-Agent: %s\r\nContent-Length: 5\r\n\r\nhello"
 	for _, ua := range uas {
-		new_payload, err := gormw.SetHttpHeader(payload, "user-agent", ua)
+		new_payload, err := SetHttpHeader(payload, "user-agent", ua)
 		if err != nil {
 			t.Errorf(err.Error())
 		}
@@ -99,7 +98,7 @@ func TestSetHttpHeader(t *testing.T) {
 	}
 
 	expected = "GET / HTTP/1.1\r\nX-Test: test\r\nUser-Agent: Golang\r\nContent-Length: 5\r\n\r\nhello"
-	new_payload, err := gormw.SetHttpHeader(payload, "X-Test", "test")
+	new_payload, err := SetHttpHeader(payload, "X-Test", "test")
 	if err != nil {
 		t.Errorf(err.Error())
 	} else if new_payload != expected {
@@ -107,7 +106,7 @@ func TestSetHttpHeader(t *testing.T) {
 	}
 
 	expected = "GET / HTTP/1.1\r\nX-Test2: test2\r\nX-Test: test\r\nUser-Agent: Golang\r\nContent-Length: 5\r\n\r\nhello"
-	new_payload, err = gormw.SetHttpHeader(new_payload, "X-Test2", "test2")
+	new_payload, err = SetHttpHeader(new_payload, "X-Test2", "test2")
 	if err != nil {
 		t.Errorf(err.Error())
 	} else if new_payload != expected {
